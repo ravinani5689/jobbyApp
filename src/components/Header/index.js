5a26355fac4d5f6ca346a31e2667f7254364a3a6
Ravi <rteja2257@gmail.com>
2025-01-23 10:33:05 +0000

    Jobby

---
+++ src/components/Header/index.js
@@ -0,0 +1,59 @@
+import {Link, withRouter} from 'react-router-dom'
+import {AiFillHome} from 'react-icons/ai'
+import {BsFillBriefcaseFill} from 'react-icons/bs'
+import {FiLogOut} from 'react-icons/fi'
+import Cookies from 'js-cookie'
+
+const Header = props => {
+  const {history} = props
+  const onClickLogout = () => {
+    Cookies.remove('js-token')
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <Link to="/">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+            className="home-website-logo"
+          />
+        </Link>
+
+        <ul className="nav-items">
+          <li>
+            <Link to="/" className="nav-link-lg">
+              Home
+            </Link>
+            <Link to="/" className="nav-link-sm">
+              <Link to="/" className="nav-link-sm">
+                <AiFillHome className="small-header-icons" />
+              </Link>
+            </Link>
+          </li>
+          <li>
+            <Link to="/jobs" className="nav-link-lg">
+              Jobs
+            </Link>
+            <Link to="/jobs" className="nav-link-sm">
+              <BsFillBriefcaseFill className="small-header-icons" />
+            </Link>
+          </li>
+          <li className="logout-btn-list-item-small">
+            <button
+              type="button"
+              className="logout-button-lg"
+              onClick={onClickLogout}
+            >
+              Logout
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
